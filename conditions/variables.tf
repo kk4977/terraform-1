13e954e3ed2e84b6d435fbf16f5eacb04107b4f6
SIVA KUMAR KADALI <kadalisivakumar789@gmail.com>
2024-05-08 12:28:24 +0530

    Code Updated  by sivakumar

--- conditions/variables.tf
+++ conditions/variables.tf
@@ -1,15 +1,14 @@
 variable "image_id" {
-    type = string
-    default = "ami-090252cbe067a9e58"
-    description = "RHEL AMI id"
-  
+  type        = string #optional
+  default     = "ami-090252cbe067a9e58" #optional
+  description = "RHEL-9 AMI ID" #optional
 }
-variable "instance_type"{
-    type = string
+
+variable "instance_type" {
     default = "t3.micro"
-}
-variable "instance_name" {
     type = string
+}
+
+variable "instance_name" {
     default = "backend"
-  
 }
